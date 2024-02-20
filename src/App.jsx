e2b2670063a5265a3a1426adaa662f561b72d65e
Omkar Vaidya <vaidyaomkar03@gmail.com>
2024-02-20 21:16:13 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>eShoppy - Ultimate E-Commerce Tool</h1>
+      <div className="card">
+        <p>Basic Templete For eShoppy Website</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
